ef4b7f99a128ded374f1f6c9bb83d6d2cc1ae714
konglong <1016361268@qq.com>
2022-07-08 07:38:56 +0800

    生命周期函数

--- src/components/01-App.vue
+++ src/components/01-App.vue
@@ -1,11 +1,27 @@
 <template>
-  展示第一个组件
+  生命周期函数
+  setup, onBeforeMount, onMounted
 </template>
 
-<script lang="ts" setup>
-import { reactive, toRefs, ref } from "vue"
+<script lang="ts">
+import { defineComponent, onBeforeMount, onMounted } from "vue"
+export default defineComponent({
+  /**
+   * setup 是生命周期函数，它代替了 beforeCreate 和 created
+   */
+  setup() {
+    console.log('setup执行了1');
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount执行了2');
+    })
+
+    onMounted(() => {
+      console.log('onMounted执行了3');
+    })
+  }
+})
 </script>
 
 <style lang="less" scoped>
-  
 </style>
